5843379075f55237d7e54eaf9d00b16edd2ec753
menduz <github@menduz.com>
2023-09-13 22:51:54 +0000

    typos

--- src/platform/native/Core.zig
+++ src/platform/native/Core.zig
@@ -311,8 +311,8 @@ pub fn init(
     core.current_border = options.border;
     core.last_border = true;
 
-    core.current_resizeable = options.resizeable;
-    core.last_resizeable = options.resizeable;
+    core.current_resizeable = options.resizable;
+    core.last_resizeable = options.resizable;
 
     core.current_headless = options.headless;
     core.last_headless = core.current_headless;
@@ -619,7 +619,7 @@ pub fn update(self: *Core, app: anytype) !bool {
             switch (self.current_display_mode) {
                 .windowed => {
                     self.window.setAttrib(.decorated, current_border);
-                    self.window.setAttrib(.resizeable, current_resizeable);
+                    self.window.setAttrib(.resizable, current_resizeable);
                     self.window.setAttrib(.floating, false);
                     self.window.setMonitor(
                         null,
@@ -650,7 +650,7 @@ pub fn update(self: *Core, app: anytype) !bool {
                     if (monitor) |m| {
                         const video_mode = m.getVideoMode();
                         if (video_mode) |v| {
-                            self.window.setAttrib(.resizeable, false);
+                            self.window.setAttrib(.resizable, false);
                             self.window.setMonitor(m, 0, 0, v.getWidth(), v.getHeight(), null);
                         }
                     }
@@ -677,7 +677,7 @@ pub fn update(self: *Core, app: anytype) !bool {
                         if (video_mode) |v| {
                             self.window.setAttrib(.decorated, false);
                             self.window.setAttrib(.floating, true);
-                            self.window.setAttrib(.resizeable, false);
+                            self.window.setAttrib(.resizable, false);
                             self.window.setMonitor(null, 0, 0, v.getWidth(), v.getHeight(), null);
                         }
                     }
@@ -692,10 +692,10 @@ pub fn update(self: *Core, app: anytype) !bool {
             if (self.current_display_mode != .borderless) self.window.setAttrib(.decorated, self.current_border);
         }
 
-        // Resizeable changes only affect windowed mode
+        // Resizable changes only affect windowed mode
         if (self.current_resizeable != self.last_resizeable) {
             self.last_resizeable = self.current_resizeable;
-            if (self.current_display_mode == .windowed) self.window.setAttrib(.resizeable, self.current_resizeable);
+            if (self.current_display_mode == .windowed) self.window.setAttrib(.resizable, self.current_resizeable);
         }
 
         // Headless changes
@@ -852,10 +852,10 @@ pub fn setResizeable(self: *Core, value: bool) void {
 }
 
 // May be called from any thread.
-pub fn resizeable(self: *Core) bool {
+pub fn resizable(self: *Core) bool {
     self.state_mu.lock();
     defer self.state_mu.unlock();
-    return self.resizeable;
+    return self.resizable;
 }
 
 // May be called from any thread.
